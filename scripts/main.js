58a9339947ad0048d360c43c691c2ebc18232a7d
Adam Heller <adam@heller.photo>
2021-01-10 11:20:18 -0500

    added initial localstorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 
 jQuery(() => {
+
+    const ITEM_KEY = 'tm_item_key_v1'; // stored v1 items
+    const NAME_KEY = 'tm_name_key_v1'; // stored v1 of name
+
+
     const inGameName = $('#inGameName');
     const addItem = $('#addItemBtn'); // add item button
     const generateBtn = $('#generateBtn'); // submit / generate button
@@ -12,27 +17,15 @@ jQuery(() => {
     const wantForItem = $('#wantForItem'); // what the want is for this given item
     const selectAllText = $('#selectAllText'); // select all text in the generated box
 
-
-    // https://stackoverflow.com/a/1173319
-    function selectText(containerid) {
-        if (document.selection) { // IE
-            var range = document.body.createTextRange();
-            range.moveToElementText(document.getElementById(containerid));
-            range.select();
-        } else if (window.getSelection) {
-            var range = document.createRange();
-            range.selectNode(document.getElementById(containerid));
-            window.getSelection().removeAllRanges();
-            window.getSelection().addRange(range);
-        }
-    }
-
+    // attempt to load from disk
+    getItems();
+    getName();
+    
     selectAllText.on('click', (e) => {
         e.preventDefault();
         selectText('generatedText');
     });
 
-    let saveJson = []; // TODO: implement save functionality
 
     clearAllBtn.on('click', (e) => {
         e.preventDefault();
@@ -50,6 +43,75 @@ jQuery(() => {
     generateBtn.on('click', generateClicked);
     
     addItem.on('click', addItemClicked);
+
+    function getName() {
+        localforage.getItem(NAME_KEY, (err, val) => {
+            if (err) {
+                localforage.setItem(NAME_KEY, '', (err) => {
+                    // pass
+                });
+            }
+            inGameName.val(val);
+        });
+    }
+
+    function getItems() {
+        localforage.getItem(ITEM_KEY, (err, val) => {
+            if (err) {
+                localforage.setItem(ITEM_KEY, [], (err) => {
+                    // pass
+                });
+            }
+            for(let i = 0; i < val.length; i++) {
+                loadItem(val[i]);
+            }
+        })
+    }
+
+    function loadItem(xData) {
+        // [ITEM, WANT]
+        const i = new Item(xData[0]);
+        const want = xData[1];
+        const wantString = makeWantString(want);
+        items.html(items.html() + '<li>' + i.toString() + wantString + '</li>');
+        generatedArea.html(generatedArea.html() + i.toString() + wantString + '\n');
+
+    }
+
+    function save(xData) {
+        localforage.getItem(ITEM_KEY, (err, items) => {
+            if (err) {
+                console.error('unable to getItem(ITEM_KEY) during save');
+                return;
+            }
+            items.push(xData);
+            localforage.setItem(ITEM_KEY, items, (err) => {
+                if (err) {
+                    console.error('unable to save new copy of items during save');
+                }
+            })
+        });
+    }
+
+    function saveInfo(xData) {
+        localforage.setItem(NAME_KEY, xData, (err) => {
+            if (err) console.error(err);
+        });
+    }
+
+    // https://stackoverflow.com/a/1173319
+    function selectText(containerid) {
+        if (document.selection) { // IE
+            var range = document.body.createTextRange();
+            range.moveToElementText(document.getElementById(containerid));
+            range.select();
+        } else if (window.getSelection) {
+            var range = document.createRange();
+            range.selectNode(document.getElementById(containerid));
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+        }
+    }
 
     function addItemClicked(e) {
         e.preventDefault();
@@ -71,9 +133,11 @@ jQuery(() => {
         }
         
         let i = new Item(itmJson);
-        const wantString = makeWantString();
+        const wantString = makeWantString(wantForItem.val());
         items.html(items.html() + '<li>' + i.toString() + wantString + '</li>');
-        saveJson.push([i.toJson(), wantForItem.val()]);
+
+        save([i.toJson(), wantForItem.val()]);
+
         generatedArea.html(generatedArea.html() + i.toString() + wantString + '\n');
 
 
@@ -81,9 +145,9 @@ jQuery(() => {
         wantForItem.val('');
     }
 
-    function makeWantString() {
-        if (wantForItem.val() == null || wantForItem.val() == '') return '';
-        return ' [W] ' + wantForItem.val();
+    function makeWantString(xVal) {
+        if (xVal == null || xVal == '') return '';
+        return ' [W] ' + xVal;
     }
 
     function generateClicked(e) {
@@ -95,6 +159,7 @@ jQuery(() => {
         str += 'Contact here';
         if (inGameName.val() != '') {
             str += ' or /w ' + inGameName.val() + ' in game.';
+            saveInfo(inGameName.val());
         }
         str += '\n';
         str += oldHtml;
@@ -104,9 +169,8 @@ jQuery(() => {
     }
 
     function generateAd() {
-        return 'This trade generated by Trade Manager at https://hellercommaa.github.io';
+        return 'This trade generated by Trade Manager at hellercommaa.github.io';
     }
-
 
     function makeAlert(xMsg) {
         alertArea.text(xMsg);
@@ -114,4 +178,3 @@ jQuery(() => {
     }
 
 });
-
